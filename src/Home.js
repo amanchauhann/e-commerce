8ff9e5fcf1bea566feece2579c2637feeda8041c
Aman Chauhan <amangotpro@gmail.com>
2022-03-20 17:59:24 +0530

    progress: home

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,9 @@ import './Home.css'
 
 function Home() {
     return (
-        <div>
+        <div className='home'>
             <div className="padder"></div>
-            <img src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/18/f2c675ae-e1c7-4657-b987-b809f0f654121647579910620-Weekend-sale_DK.jpg" />
+            <img className='image' src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/18/f2c675ae-e1c7-4657-b987-b809f0f654121647579910620-Weekend-sale_DK.jpg" />
         </div>
     )
 }
