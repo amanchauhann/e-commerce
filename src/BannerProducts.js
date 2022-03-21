092b0b47c4cccd2bdf5295527c383ff474816bf7
Aman Chauhan <amangotpro@gmail.com>
2022-03-21 11:49:50 +0530

    style: grid

--- src/BannerProducts.js
+++ src/BannerProducts.js
@@ -3,13 +3,15 @@ import './BannerProducts.css';
 function BannerProducts() {
     return (
         <div className="bannerProducts">
-            <div className="mainCard">
+            <div className='mainCard'>
                 <div className='topCard'>
-                <img src="https://cdn.britannica.com/06/200006-131-ABB681CF/Leonardo-da-Vinci-Italian-Renaissance-Florence-Engraving-1500.jpg" /> </div>
-                <div className="bottomCard">
-                    <div className="title"></div>
-                    <div className="discount"></div>
+                    <img src='https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/15743592/2021/10/6/90169c19-94c1-47d4-a6f1-2a7b1f346fcd1633482602050HIGHLANDERMenWhiteSlimFitFloralOpaquePrintedCasualShirt1.jpg' />
                 </div>
+                <div className='bottomCard'>
+                    <div className='title'>Trendy Styles</div>
+                    <div className='discount'>Upto 60% off</div>
+                </div>
+
             </div>
         </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,14 @@ function Home() {
             <img className='main-image' src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/18/f2c675ae-e1c7-4657-b987-b809f0f654121647579910620-Weekend-sale_DK.jpg" />
             <img className='bannerImage' src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/3/19/f52556ea-783c-4eab-8bb9-a3b909b634451647665260055-DK-b1g27pm.jpg' />
             </div>
-            <BannerProducts />
+            <div className='bannerProductsContainer'>
+                <BannerProducts />
+                <BannerProducts />
+                <BannerProducts />
+                <BannerProducts />
+                <BannerProducts />
+                <BannerProducts />
+            </div>
         </div>
     )
 }
